e5b40f497099087a3f25e0dee1386e5b0ff75bb6
JustinG <jg.garandel@hotmail.fr>
2025-02-18 14:44:51 +0100

    ajout composer symfony/form pour formulaire

---
+++ src/Form/UserProfileType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;    
+use Symfony\Component\Form\Extension\Core\Type\TextType;     
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    //construction formulaire modification de profil user
+    $builder
+      // email avec type form
+      ->add('email', EmailType::class, [
+        'label' => 'Email',
+        'attr' => ['class' => 'form-control']
+      ])
+      // prénom
+      ->add('firstname', TextType::class, [
+        'label' => 'Prénom',
+        'attr' => ['class' => 'form-control']
+      ])
+      // nom
+      ->add('name', TextType::class, [
+        'label' => 'Nom',
+        'attr' => ['class' => 'form-control']
+      ])
+      // téléphone si besoin
+      ->add('phone_number', TelType::class, [
+        'label' => 'Téléphone',
+        'required' => false,
+        'attr' => ['class' => 'form-control']
+      ]);
+  }
+
+  // Configure les options du formulaire
+  public function configureOptions(OptionsResolver $resolver)
+  {
+      // Lie le formulaire à l'entité User
+      $resolver->setDefaults([
+          'data_class' => User::class,
+      ]);
+  }
+}
